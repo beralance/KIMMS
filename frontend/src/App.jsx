1b06c35abe133f6f26ebc0e0b0f38b22829a3225
selfstudy1153-dev <selfstudy1153@gmail.com>
2025-09-04 00:13:34 +0800

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import AppRoutes from './router/routes'
 import { useEffect } from 'react';
 import { initializeAccounts } from './utils/Accounts'
-import SnackbarWrapper from './components/SnackBarWrapper'
+import SnackbarWrapper from './components/SnackbarWrapper'
 
 function App() {
     useEffect(() => {
