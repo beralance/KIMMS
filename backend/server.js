bf07f1cbbbf6b7ef79ea26f363df9703228e0f65
selfstudy1153-dev <selfstudy1153@gmail.com>
2025-09-30 21:06:41 +0800

    mongoose.connect modification on server.js

--- backend/server.js
+++ backend/server.js
@@ -111,7 +111,7 @@ const createAdmin = async () => {
 
 // MongoDB + Server
 const PORT = process.env.PORT || 5000
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI) // removed: connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
         console.log('✅ MongoDB connected')
         await createAdmin();
