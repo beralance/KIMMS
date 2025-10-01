33375af8b2aa0ac22a76932399421f0ecfbda73c
selfstudy1153-dev <selfstudy1153@gmail.com>
2025-10-01 20:14:10 +0800

    VITE_FRONTEND_URL.env

--- frontend/src/features/admin/inventory/components/PhysicalCodeDisplayer.jsx
+++ frontend/src/features/admin/inventory/components/PhysicalCodeDisplayer.jsx
@@ -19,7 +19,7 @@ export default function PhysicalCodeDisplayer({id, name, category, status, image
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
     const printRef = useRef();
-    const API_URL = import.meta.env.FRONTEND_URL;
+    const API_URL = import.meta.env.VITE_FRONTEND_URL;
     const handlePrint = () => {
         const printContents = printRef.current.innerHTML;
         const printWindow = window.open("", "_blank");
